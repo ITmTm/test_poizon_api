efcf9cff8c003e645dcb45bef6a6803c79272d8f
Timur <Stafr95@mail.ru>
2024-08-13 12:19:32 +0500

    Creating selectors

---
+++ src/redux/selector.js
@@ -0,0 +1,6 @@
+export const selectText = state => state.text.text;
+
+export const selectCorrectChars = state => state.text.correctChars;
+export const selectUserInput = state => state.input.userInput;
+
+export const selectErrors = state => state.input.errors;
