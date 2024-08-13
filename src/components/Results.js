cf02b80ff597251a146224373f228666b7ac3e90
Timur <Stafr95@mail.ru>
2024-08-13 12:21:33 +0500

    Using selectors

--- src/components/Results.js
+++ src/components/Results.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import { selectCorrectChars, selectErrors } from "../redux/selector";
 
 import styles from '../styles/Results.module.scss';
 
 const Results = ({ handleRestart }) => {
-	const correctChars = useSelector((state) => state.text.correctChars);
-	const errors = useSelector((state) => state.input.errors);
+	const correctChars = useSelector(selectCorrectChars);
+	const errors = useSelector(selectErrors);
 
 	const calculateWPM = () => {
 		const wordsTyped = correctChars / 5;  // Среднее кол-во символов в слове
